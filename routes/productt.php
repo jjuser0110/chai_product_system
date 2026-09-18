48cef173eb90c7f68594878f5bcdf92941c7ee91
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-09-18 23:33:05 +0800

    a

--- routes/productt.php
+++ routes/productt.php
@@ -7,7 +7,7 @@ Route::prefix('/productt')->as('productt.')->middleware(['auth'])->group(functio
     Route::get('/index', 'ProducttController@index')->name('index');
     Route::get('/create', 'ProducttController@create')->name('create');
     Route::post('/store', 'ProducttController@store')->name('store');
-    Route::get('/edit/{productt}', 'ProducttController@edit')->name('edit');
-    Route::post('/update/{productt}', 'ProducttController@update')->name('update');
-    Route::get('/destroy/{productt}', 'ProducttController@destroy')->name('destroy');
+    Route::get('/edit/{product}', 'ProducttController@edit')->name('edit');
+    Route::post('/update/{product}', 'ProducttController@update')->name('update');
+    Route::get('/destroy/{product}', 'ProducttController@destroy')->name('destroy');
 });
